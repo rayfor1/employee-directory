5ecb25332e87948a09bd908a75549eae15b14070
Raymond Anuforo Jr <anufororaymond@yahoo.com>
2020-09-16 11:56:25 -0400

    installed react-moment npm package

--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import "../styles/Employee.css";
+import Moment from "moment";
 
-const Employees = function (props) {
+const Employees = (props) => {
+
+  let fixedDate = Moment(props.dob).format("LL");
+
   return (
+
     <tr className="tr">
-      <td>
-        <img alt={props.firstName} src={props.icon} />
-      </td>
-      <td>
-        {props.firstName} {props.lastName}
-      </td>
+      <td><img alt={props.firstName} src={props.icon} /></td>
+      <td>{props.firstName} {props.lastName}</td>
       <td>{props.email}</td>
       <td>{props.phone} </td>
-      <td>{props.dob}</td>
+      <td>{fixedDate}</td>
     </tr>
-  );
-};
+
+  )
+
+}
 
 export default Employees;
